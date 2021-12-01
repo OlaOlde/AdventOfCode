ce6cc81a8d86e65ab88b03862219a970a66e0ab5
Ola Olde <OOLDE@volvocars.com>
2021-12-01 10:55:07 +0100

    complete puzzle 1

--- dec01/Main.py
+++ dec01/Main.py
@@ -1,4 +1,22 @@
+def howmuchincrease(list):
+    increase = 0
+    for x in range(len(list) -1):
+        next = int(list[x+1])
+        curr = int(list[x])
+        if int(list[x+1]) > int(list[x]):
+            increase += 1
+    return increase
+
+def makeSummedList(list):
+    newList = []
+    for x in range(len(list)-2):
+        temp = int(list[x]) + int(list[x+1]) + int(list[x+2])
+        newList.append(temp)
+    return newList
+
 def main():
-    print("Hello world!")
+    depths = open("input.txt").read().splitlines()
+    print(howmuchincrease(depths))
+    print(howmuchincrease(makeSummedList(depths)))
 
 main()
