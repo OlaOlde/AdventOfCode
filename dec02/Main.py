6d047bc06e3c8b29fec34a8067d2dc801610b40e
Ola Olde <OOLDE@volvocars.com>
2021-12-02 09:49:18 +0100

    complete puzzle 2

--- dec02/Main.py
+++ dec02/Main.py
@@ -1,4 +1,34 @@
-def main():
-    print("Hello world!")
 
+def secondPuzzle(input):
+    aim = 0
+    vPos = 0
+    hPos = 0
+    for command in input:
+        x = command.split()
+        steps = int(x[1])
+        if x[0] == 'forward':
+            hPos = hPos + steps
+            vPos = vPos + steps * aim
+        elif x[0] == 'up':
+            aim = aim - steps
+        elif x[0] == 'down':
+            aim = aim + steps
+    print("answer to second puzzle: " + str(vPos * hPos))
+def main():
+    inputs = open("input.txt").read().splitlines()
+    verticalPos = 0
+    horizontalPos = 0
+    aim = 0
+    for command in inputs:
+        x = command.split()
+        steps = int(x[1])
+        #dir = str(x[0])
+        if x[0] == 'forward':
+            horizontalPos = horizontalPos + steps
+        elif x[0] == 'up':
+            verticalPos = verticalPos - steps
+        elif x[0] == 'down':
+            verticalPos = verticalPos + steps
+    print("answer to first puzzle: "+ str(verticalPos*horizontalPos))
+    secondPuzzle(inputs)
 main()
