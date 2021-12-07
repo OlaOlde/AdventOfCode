49b9b4c2b70babb5367853823e04b90d726aea56
Ola Olde <OOLDE@volvocars.com>
2021-12-07 10:25:45 +0100

    first part of dec03

--- dec03/Main.py
+++ dec03/Main.py
@@ -1,4 +1,10 @@
 def main():
-    print("Hello world!")
-
+    inputs = open("input.txt").read().splitlines()
+    gamma = epsilon = 0
+    for x in range(len(inputs[0])):
+        if sum([int(i[x]) for i in inputs]) > 500:
+            gamma = gamma + 2**(11-x)
+        else:
+            epsilon = epsilon + 2**(11-x)
+    print(gamma*epsilon)
 main()
