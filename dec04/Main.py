1723a819052af89404a704a1d2bedb800606193c
Ola Olde <OOLDE@volvocars.com>
2021-12-09 09:42:33 +0100

    solve dec04

--- dec04/Main.py
+++ dec04/Main.py
@@ -1,4 +1,44 @@
+import numpy as np
+def score(board, nbrs):
+    shape = (5, 5)
+    matrix = np.zeros(shape)
+    for q in range(5):
+        row = board[q].split()
+        for z in range(5):
+            matrix[[q],[z]]= int(row[z])
+    checkMatrix = np.zeros(shape)
+    ind = 0
+    for x in nbrs:
+        y = np.where(matrix == int(x))
+        ind = ind + 1
+        if y[0].size != 0:
+            checkMatrix[y[0],y[1]] = 1
+            if(checkBingo(checkMatrix)):
+                tempMatrix = 1-checkMatrix
+                scoreMatrix = tempMatrix*matrix
+                score = sum(sum(scoreMatrix))*int(x)
+
+                return int(score), ind
+def checkBingo(matrix):
+    s = matrix.shape
+    bingoCol = np.where(matrix.sum(axis=0) == s[0])
+    bingoRow = np.where(matrix.sum(axis=1) == s[1])
+    if len(bingoRow[0]) != 0 or len(bingoCol[0]) != 0:
+        return True
+    else:
+        return False
 def main():
-    print("Hello world!")
+    boards = open("boards.txt").read().splitlines()
+    numbers = open("numbers.txt").read().split(',')
+    scores = []
+    indices = []
+    for x in range(int(len(boards)/6)):
+        tempScore, ind = score(boards[(x*6):(x*6)+5],numbers)
+        scores.append(tempScore)
+        indices.append(ind)
+    minInd = np.argmin(indices)
+    maxInd = np.argmax(indices)
+    print("puzzle 1 ans: " + str(scores[minInd]))
+    print("puzzle 2 ans: " + str(scores[maxInd]))
 
 main()
