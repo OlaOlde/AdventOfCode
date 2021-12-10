d507bca4c8554daf653cfd916c28d1c1eee1568c
Ola Olde <OOLDE@volvocars.com>
2021-12-10 10:17:35 +0100

    working on dec05

--- dec05/Main.py
+++ dec05/Main.py
@@ -1,4 +1,28 @@
-def main():
-    print("Hello world!")
+import re
 
+
+def fixInput(input):
+    xValues = []
+    yValues = []
+    for x in input:
+        values = re.findall("[0-9]{1,3}", x)
+        xValues.append(int(values[0]))
+        xValues.append(int(values[2]))
+        yValues.append(int(values[1]))
+        yValues.append(int(values[3]))
+    return xValues, yValues
+
+    return
+def removeDiag(lines):
+    return
+def getMatrix(lines):
+    return
+def main():
+    input = open("input.txt").read().splitlines()
+   # x = map(fixInput, input)
+   # print(list(x))
+    #x, y = map(fixInput, input) #(input[0])
+    x, y = fixInput(input)
+    print(x)
+    print(y)
 main()
