1149cb493e1bdaef785472fc3d6aa0bcb489314b
Ola Olde <OOLDE@volvocars.com>
2021-12-15 15:56:39 +0100

    continuation of dec05

--- dec05/Main.py
+++ dec05/Main.py
@@ -13,16 +13,28 @@ def fixInput(input):
     return xValues, yValues
 
     return
-def removeDiag(lines):
-    return
+def removeDiag(x, y):
+    ind = len(x)
+    for i in range(0,len(x)-2,2):
+        ind = ind - i
+        if x[ind] - x[ind - 1] != 0 and y[ind]-y[ind - 1] != 0:
+            x[ind-1:ind] = []
+            y[ind-1:ind] = []
+    return x, y
 def getMatrix(lines):
     return
 def main():
     input = open("input.txt").read().splitlines()
    # x = map(fixInput, input)
    # print(list(x))
-    #x, y = map(fixInput, input) #(input[0])
+    #x, y = fixUnput(input) #(input[0])
     x, y = fixInput(input)
-    print(x)
-    print(y)
+    #print(x)
+    #print(y)
+    print(len(x))
+   # z = [0, 1,2,3,4,5,6,7,9]
+   # print(z)
+   # print(len(z))
+
+   # newx, newy = removeDiag(x,y)
 main()
